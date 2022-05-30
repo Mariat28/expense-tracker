550f22e3413025e7cb03a816c2ab9d5cddabbf17
Mariat28 <mariatndagire28@gmail.com>
2022-05-30 17:51:39 +0300

    event handling

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -2,6 +2,9 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../ui/Card";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log('clicked!!!!')
+  }
   return (
     <div className="w-full">
       <Card className="flex justify-start bg-slate-500 text-white  mx-2">
@@ -10,8 +13,9 @@ const ExpenseItem = (props) => {
             <ExpenseDate date={props.date}/>
             <h2 className="font-semibold">{props.title}</h2>
           </div>
-          <div className="flex bg-white p-1 rounded-md text-slate-500 cursor-pointer">
+          <div className="flex bg-white p-1 rounded-md text-slate-500 cursor-pointer gap-2">
             <small>${props.amount}</small>
+            <button onClick={clickHandler}>Change Title</button>
           </div>
         </div>
       </Card>
